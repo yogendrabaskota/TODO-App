2dbd099ed18afc2fc43fc8ba376e5fb06b6fcfca
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-04-18 22:54:45 +0545

    initial phase

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key?key}) :super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TODO APP',
+      home: Home(),
+    );
+  }
+}
+
+//class SystemChrome {
+//  static void setSystemUIOverlayStyle(SystemUiOverlayStyle SystemUiOverlayStyle) {}
+//}
+
+//class SystemUiOverlayStyle {
+//}

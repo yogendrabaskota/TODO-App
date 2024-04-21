7a9dc94c1e15d1058bda08c7ac74343a6cd28d96
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-04-21 14:37:16 +0545

    todo

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,53 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:todo/constants/colors.dart';
 
+import '../model/todo.dart';
+import '../constants/colors.dart';
 
-class ToDoItems extends StatelessWidget {
-  const ToDoItems({super.key});
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem({
+    Key? key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: ()  {},
+        onTap: () {
+          // print('Clicked on Todo Item.');
+          onToDoChanged(todo);
+        },
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
           ),
-          tileColor: Colors.white,
-          leading: Icon(
-            Icons.check_box,
-            color: tdBlue,
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
           ),
-          title: Text(
-            'Check mail',
-            style: TextStyle(
-              fontSize: 16,
-              color: tdBlack,
-              decoration: TextDecoration.lineThrough,
-            ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              // print('Clicked on delete icon');
+              onDeleteItem(todo.id);
+            },
           ),
-          trailing: Container(
-            height: 35,
-            width: 35,
-            decoration: BoxDecoration(
-              color: tdRed,
-              borderRadius: BorderRadius.circular(5),
-
-            ),
-            child: IconButton(
-              color: Colors.white,
-              iconSize: 18,
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                
-              },
-            ),
-
-          ),
-
+        ),
       ),
-      );
+    );
   }
 }
-
-
